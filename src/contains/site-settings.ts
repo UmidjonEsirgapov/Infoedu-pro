0ef74088dd2c4873bf2626993d8bd11521b5a604
Umidjon Esirgapov <umidjon.me@gmail.com>
2025-11-29 02:19:02 +0500

    yangilik

--- src/contains/site-settings.ts
+++ src/contains/site-settings.ts
@@ -9,9 +9,36 @@ type RecursivePartial<T> = {
 
 type PartialExcept<T, K extends keyof T> = RecursivePartial<T> & Pick<T, K>
 
+/**
+ * Recursively replaces [year] placeholder with current year in all string values
+ */
+function replaceYearInObject(obj: any): any {
+	if (typeof obj === 'string') {
+		const currentYear = new Date().getFullYear()
+		return obj.replace(/\[year\]/g, currentYear.toString())
+	}
+	if (Array.isArray(obj)) {
+		return obj.map(item => replaceYearInObject(item))
+	}
+	if (obj && typeof obj === 'object') {
+		const result: any = {}
+		for (const key in obj) {
+			if (Object.prototype.hasOwnProperty.call(obj, key)) {
+				result[key] = replaceYearInObject(obj[key])
+			}
+		}
+		return result
+	}
+	return obj
+}
+
+const mergedSettings = _.merge({}, defaultSiteSettingsJson, siteSettingsJson)
+
 export const NC_SITE_SETTINGS: RecursivePartial<
 	typeof defaultSiteSettingsJson
-> = _.merge({}, defaultSiteSettingsJson, siteSettingsJson)
+> = replaceYearInObject(mergedSettings) as RecursivePartial<
+	typeof defaultSiteSettingsJson
+>
 
 export const IS_CHISNGHIAX_DEMO_SITE =
 	process.env.NEXT_PUBLIC_IS_CHISNGHIAX_DEMO_SITE === 'true'
