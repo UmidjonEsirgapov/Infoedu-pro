d62fec43914c240327aa28783001a24bddc1d2fb
Umidjon <umidjon.me@gmail.com>
2026-02-06 03:01:52 +0500

    Fix CSP: Add YouTube to script-src and frame-src directives

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -125,7 +125,7 @@ export default class Document extends NextDocument {
 
 					<meta
           httpEquiv="Content-Security-Policy"
-          content="script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yastatic.net https://cdn.onesignal.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com https://www.googletagservices.com https://ep2.adtrafficquality.google" 
+          content="script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yastatic.net https://cdn.onesignal.com https://www.googletagmanager.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://partner.googleadservices.com https://tpc.googlesyndication.com https://www.googletagservices.com https://ep2.adtrafficquality.google https://www.youtube.com; frame-src 'self' https://www.youtube.com" 
         />
          
         <script
